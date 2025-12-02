6c553713ea3f2bdfc0b0b5cddfc9af837d5c4338
SamAdeSan <samadesan@alu.edu.gva.es>
2025-12-02 09:23:25 +0000

    TODOS los primeros pasos de la web personal acabados

--- src/Controller/PageController.php
+++ src/Controller/PageController.php
@@ -2,23 +2,32 @@
 
 namespace App\Controller;
 
+use Symfony\Bridge\Doctrine\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class PageController extends AbstractController
 {
-    #[Route('/page', name: 'app_page')]
-    public function page(): Response
-    {
-        return $this->render('page/index.html.twig', [
-            'controller_name' => 'PageController',
-        ]);
-    }
-
     #[Route('/', name: 'index')]
     public function index(): Response
     {
-        return $this->render('');
+        return $this->render('page/index.html.twig', []);
+    }
+    #[Route('/about', name: 'about')]
+    public function about(): Response
+    {
+        return $this->render('page/about.html.twig', []);
+    }
+    #[Route('/archieves', name: 'archieves')]
+    public function blog(): Response
+    {
+        return $this->render('page/archieves.html.twig', []);
+    }
+    #[Route('/contact', name: 'contact')]
+    public function contact(): Response
+    {
+        return $this->render('page/contact.html.twig', []);
     }
 }
